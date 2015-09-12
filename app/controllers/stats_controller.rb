9fcd4079989ed476b5449646b4273594bc5495b2
Alberto Plata <albertoplata@Albertos-MacBook-Pro.local>
2015-09-11 21:47:23 -0700

    Changing Stats to Rankings

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,7 @@
 class StatsController < ApplicationController
   def index
     @stats = Stat.first #theres only one stat set get the first!
-    @title = "Stats"
+    @title = "Rankings"
     session[:school_id_for_major] = nil
   end
 end
