9dfe654d2097ab6ed0d662dd76c7e8c48deb6407
Alberto Plata <albertoplata@Albertos-MacBook-Pro.local>
2016-01-01 22:49:09 -0800

    adding buttons from home page

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -12,6 +12,16 @@ class StatesController < ApplicationController
         redirect_to new_user_session_path and return
       end
     end
+    if params[:money_flow] != nil
+      cleanup_post_flow
+      session[:major_id_for_school] = nil
+      flash[:notice] = "Search college reviews to see what grads are earning."
+    end
+    if params[:ask_flow] != nil
+      cleanup_post_flow
+      session[:major_id_for_school] = nil
+      flash[:notice] = "Search college reviews to begin asking questions."
+    end
     if params[:clr_sch] != nil
       cleanup_post_flow
       session[:major_id_for_school] = nil
