68054f26700873ffa58d1aec6276f099554d503c
Alberto Plata <albertoplata@Albertos-MacBook-Pro.local>
2016-01-07 22:15:09 -0800

    let users go further when they click on the post button

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -6,13 +6,13 @@ class StatesController < ApplicationController
     if params[:post_flow] != nil
       session[:major_id_for_school] = nil
       cleanup_autocomplete_search
-      if user_signed_in?
-        session[:executing_post_flow] = "true"
-        @small_text = "Step 1 of 4"
-      else
-        flash[:alert] = "Grads, please sign in or sign up before posting a review."
-        redirect_to new_user_session_path and return
-      end
+      # if user_signed_in?
+      session[:executing_post_flow] = "true"
+      @small_text = "Step 1 of 4"
+      # else
+      #   flash[:alert] = "Grads, please sign in or sign up before posting a review."
+      #   redirect_to new_user_session_path and return
+      # end
     end
     if params[:money_flow] != nil
       cleanup_autocomplete_search
