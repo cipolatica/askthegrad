7ef2b1e9b2370d903acf30d83ba37944e5c0ecc7
Alberto Plata <albertoplata@Albertos-MacBook-Pro.local>
2016-01-08 23:17:58 -0800

    reverting to mnormal js file

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,7 +1,7 @@
-window.onload=$(function() {
+$(function() {
     $('.accordion-toggle').click(function (e) {
         //get html query from target
-        alert("hi" );
+        //alert("hi");
 
         var element;
 
