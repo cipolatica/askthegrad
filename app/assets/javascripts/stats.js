41f5ffd77af6917b6c15df73942be84fc229fac6
Alberto Plata <albertoplata@Albertos-MacBook-Pro.local>
2016-01-08 21:27:29 -0800

    test

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,7 +1,7 @@
 $(function() {
     $('.accordion-toggle').click(function (e) {
         //get html query from target
-        alert("hi" + e.toString());
+        alert("hi" );
 
         var element;
 
