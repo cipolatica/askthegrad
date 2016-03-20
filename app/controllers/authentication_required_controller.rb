792331fe302d51224d362bb6601bb55d0b58588a
Alberto Plata <albertoplata@Albertos-MacBook-Pro.local>
2016-03-20 11:33:37 -0700

    cleaning up code

--- app/controllers/authentication_required_controller.rb
+++ app/controllers/authentication_required_controller.rb
@@ -1,6 +1,5 @@
 class AuthenticationRequiredController < ApplicationController
   def index
     @title = "Authentication Required"
-    @reg = Registration.find(session[:reg_id])
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     session[:school_id_for_major] = nil
     session[:major_id_for_school] = nil
+    session[:reg_id] = nil
     cleanup_post_flow
     cleanup_autocomplete_search
     if user_signed_in?
